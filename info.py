5a652fc13fbeca7dbc8b4ac3c22ab9a7bb6553b5
maharaja91 <135723289+maharaja91@users.noreply.github.com>
2023-06-07 21:16:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', '')).split()
-NOR_IMG = environ.get("NOR_IMG", "")
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/b856bcc3281a959e80b0e.jpg")
 NEWGRP = environ.get("NEWGRP", "")
 MELCOW_VID = environ.get("MELCOW_VID", "")
 SPELL_IMG = environ.get("SPELL_IMG", "")
