76f0aad8efbfcad5e50ee40b885c83ea4af36d30
maharaja91 <135723289+maharaja91@users.noreply.github.com>
2023-06-07 11:23:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1237,7 +1237,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
         buttons = [[
             InlineKeyboardButton('⇍ ʙᴀᴄᴋ', callback_data='start'),
-            InlineKeyboardButton('✧ ᴄᴏɴᴛᴀᴄᴛ​', url='t.me/af_x_su')
+            InlineKeyboardButton('✧ ᴄᴏɴᴛᴀᴄᴛ​', url='t.me/maharaja_91')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
