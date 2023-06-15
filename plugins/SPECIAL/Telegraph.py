902038adb78992c582030e03a04dbecde095c2c1
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-15 15:44:07 +0530

    Update Telegraph.py

--- plugins/SPECIAL/Telegraph.py
+++ plugins/SPECIAL/Telegraph.py
@@ -43,7 +43,7 @@ async def telegraph(client, message):
                         [
                             [
                                 InlineKeyboardButton(
-                                    '🎭 ⭕️ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ ⭕️', url=f'https://t.me/maharaja_91'
+                                    '🎭 ⭕️ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ ⭕️', url=f'https://t.me/B_TOWN_22'
                                 )
                             ]
                         ]
