fe3066f544db2a5978a16f5a7c30584182884c49
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-18 16:38:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1370,17 +1370,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('y back', callback_data='dash'),
             InlineKeyboardButton('h homr', callback_data='start')
          ]]
-         reply_markup = InlineKeyboardMarkup(buttons)
-            
+         reply_markup= InlineKeyboardMarkup(buttons)
          await client.edit_message_media(
              query.message.chat.id,
              query.message.id,
              InputMediaPhoto(random.choice("https://graph.org/file/28b44582b7b08c4a30892.jpg")
          )
          await query.message.edit_text(
-                  text=script.OGGY_TXT,
-                 reply_markup=reply_markup,
-                 parse_mode=enums.ParseMode.HTML
+             text=script.OGGY_TXT,
+             reply_markup=reply_markup,
+             parse_mode=enums.ParseMode.HTML
          )
         
     elif query.data == "setting_btn":
